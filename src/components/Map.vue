41ee0919d59101d2187b42631dcf840da66b2880
Dylan <dylan.cobean@gmail.com>
2021-02-02 01:52:37 -0700

    Setup | Mapbox-Geocoder | Added Defalt Geocoder

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,7 +5,11 @@
 </template>
 
 <script>
-import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 export default {
   mounted() {
@@ -14,7 +18,7 @@ export default {
   data() {
     return {
       map: null
-    }
+    };
   },
   methods: {
     initializeMap() {
@@ -26,6 +30,32 @@ export default {
         center: [-122.2646, 37.4956], // starting position [lng, lat]
         zoom: 11 // starting zoom
       });
+
+      //Add geocoder
+      const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        // types: "poi",
+        // render: function(item) {
+        //   // extract the item's maki icon or use a default
+        //   var maki = item.properties.maki || "marker";
+        //   return (
+        //     "<div class='geocoder-dropdown-item'><img class='geocoder-dropdown-icon' src='https://unpkg.com/@mapbox/maki@6.1.0/icons/" +
+        //     maki +
+        //     "-15.svg'><span class='geocoder-dropdown-text'>" +
+        //     item.text +
+        //     "</span></div>"
+        //   );
+        // },
+        mapboxgl: mapboxgl,
+        marker: {
+          drqaggable: false,
+          color: "red"
+        }
+      });
+      this.map.addControl(geocoder, "top-left");
+
+      geocoder.on("results");
+      //
     }
   }
 };
